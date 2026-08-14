1c0e9bac28ca87053d4182f47d73cf12ccb4ad7d
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-14 14:37:02 -0300

    Criado os atributos e as funções exibirDados e LimparDados

--- src/app/component/atleta-component/atleta-component.ts
+++ src/app/component/atleta-component/atleta-component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-atleta-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta-component.html',
   styleUrl: './atleta-component.css',
 })
-export class AtletaComponent {}
+export class AtletaComponent {
+  //DECLARANDO ATIBUTOS
+  nome = ''
+  cpf = 0
+  sexo = ''
+  cep = 0
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
+
+  //DECLARAÇÃO DE FUNÇÕES
+  exibirDados() {
+    console.log(this.nome, this.cpf, this.sexo, this.cep, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+
+    this.limparDados()
+  }
+
+  limparDados() {
+    this.nome = ''
+    this.cpf = 0
+    this.sexo = ''
+    this.cep = 0
+    this.ruaLogradouro = ''
+    this.bairro = ''
+    this.cidade = ''
+    this.uf = ''
+  }
+
+
+}
