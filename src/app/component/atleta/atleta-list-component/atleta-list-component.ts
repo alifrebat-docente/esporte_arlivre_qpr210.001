b5f24212ce431ff27f6e13f2cc45f9a21ae3c9eb
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-17 14:52:51 -0300

    Criado as funções listar e ngOninit

--- src/app/component/atleta/atleta-list-component/atleta-list-component.ts
+++ src/app/component/atleta/atleta-list-component/atleta-list-component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Atleta } from '../../../models/Atleta';
+import { AtletaService } from '../../../service/atleta-service';
 
 @Component({
   selector: 'app-atleta-list-component',
@@ -6,4 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './atleta-list-component.html',
   styleUrl: './atleta-list-component.css',
 })
-export class AtletaListComponent {}
+export class AtletaListComponent {
+
+  listaAtletas: Atleta[] = []
+
+  constructor(private listaService: AtletaService){ }
+
+  ngOnInit(){
+    this.listar()
+  }
+
+  listar(){
+    this.listaService.listarAtletas()
+    .subscribe({
+      next:(dadosAtletas)=>{
+        this.listaAtletas = [...dadosAtletas].sort((a, b) => a.nome.localeCompare(b.nome))
+      },
+      error:(msgErro)=>{
+        console.log("Erro ao listar Atletas ", msgErro)
+      }
+    })
+  }
+}
